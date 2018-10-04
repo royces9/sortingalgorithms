0706829f312d87639864d2c17df6add4e908a9ff
Royce <rrysato@gmail.com>
2018-10-04 16:10:37 -0700

    change plot to allow arguments

--- data_out.py
+++ data_out.py
@@ -17,8 +17,18 @@ def sort_data(alg, prefix, count, left_range, right_range, step_size):
 
     fp.close();
 
-    
+
 if __name__ == "__main__":
+    flag = 0;
+    for i, arg in enumerate(sys.argv):
+        if arg == "-n":
+            flag = 1;
+        elif arg == "-r":
+            flag = 2;
+        elif arg == "-e":
+            flag = 3;
+        else:
+
     alg = sys.argv[1];
 
     prefix = sys.argv[2];
--- plot.py
+++ plot.py
@@ -3,18 +3,24 @@ import numpy
 import matplotlib.pyplot
 import sys
 
-directory = sys.argv[1];
 
-fig = matplotlib.pyplot.figure();
+def plot_alg(directory, file_names):
+    fig = matplotlib.pyplot.figure();
 
-for file in os.listdir(directory):
-    algorithm = os.path.splitext(file)[0];
-    data = numpy.loadtxt(directory + file, skiprows = 1);
-    matplotlib.pyplot.plot(data[:, 0], data[:, 1], label = algorithm);
+    for file in file_names:
+        data = numpy.loadtxt(directory + file + ".dat", skiprows = 1);
+        matplotlib.pyplot.plot(data[:, 0], data[:, 1], label = file);
 
 
-matplotlib.pyplot.xlim(data[0, 0], data[-1, 0]);
-matplotlib.pyplot.ylabel("Sort time, s");
-matplotlib.pyplot.xlabel("List size");
-matplotlib.pyplot.legend();
-matplotlib.pyplot.show();
+    matplotlib.pyplot.xlim(data[0, 0], data[-1, 0]);
+    matplotlib.pyplot.ylabel("Sort time, s");
+    matplotlib.pyplot.xlabel("List size");
+    matplotlib.pyplot.legend();
+    matplotlib.pyplot.show();
+
+if __name__ == "__main__":
+    directory = sys.argv[1];
+
+    #without file extension, hardcode .dat
+    file_names = sys.argv[2:len(sys.argv)];
+    plot_alg(directory, file_names);
