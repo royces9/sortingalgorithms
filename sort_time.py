fff33edeb771a729b81219292c2b048562c291e2
Royce <rrysato@gmail.com>
2018-10-06 23:57:14 -0700

    added flag for thread count if needed

--- sort_time.py
+++ sort_time.py
@@ -1,21 +1,39 @@
 import subprocess
 import sys
 
-def sort_time(alg_name, count, size):
-    time = 0;
+def sort_time(alg_name, count, size, extra):
+    time_sum = 0;
+    time = [0] * count;
+
+    if extra:
+        extra = " -e " + extra;
 
     for i in range(0, count):
-        p = subprocess.Popen(["./" + alg_name + " -f 2 -s " + size], shell = True, stdout = subprocess.PIPE);
-        split = ((p.stdout.read()).decode("utf-8")).split(" ");
-        time += float(split[2]);
+        input_str  = "./" + alg_name + " -f 2 -s " + size + extra;
+        p = subprocess.Popen(input_str, shell = True, stdout = subprocess.PIPE);
+        time[i] = float(p.stdout.read().decode("utf-8").split(" ")[2]);
+        time_sum += time[i];
 
-    return time / count;
+    avg = time_sum / count;
+    temp = 0;
+    for i in range(0, count):
+        temp += ((time[i] - avg) ** 2)
+
+    std = temp / (count - 1);
+
+    return [avg, std];
 
 
 if __name__ == "__main__":
     alg = sys.argv[1];
     count = int(sys.argv[2]);
     size = sys.argv[3];
-    average = sort_time(alg, count, size);
 
-    print(alg + ": ", average);
+    if len(sys.argv) == 5:
+        extra = sys.argv[4];
+    else:
+        extra = "";
+
+    average, std = sort_time(alg, count, size, extra);
+
+    print(alg + ": ", average, ", ", std);
