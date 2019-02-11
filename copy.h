2a59dd8ca8b7540e316419bfde4c64c792042743
Royce <rrysato@gmail.com>
2019-02-10 23:19:03 -0800

    i think this is all

---
+++ copy.h
@@ -0,0 +1,6 @@
+#ifndef COPY
+#define COPY
+
+void copy(void *src, void *dest, int size_e);
+
+#endif //COPY
---
+++ swap.h
@@ -0,0 +1,6 @@
+#ifndef SWAP
+#define SWAP
+
+void swap(void *a, void *b, int size_e);
+
+#endif //SWAP
