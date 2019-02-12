86188cba5083fa0e2ed3965ba1124f21fbfe4997
Royce <rrysato@gmail.com>
2019-02-11 21:16:41 -0800

    try to make this a bit better?

--- sort/MSDradix.c
+++ sort/MSDradix.c
@@ -26,9 +26,22 @@ int highest_bit(void *array, int size_a, int size_e, int (*compare)(void *, void
 
 
 //MSD radix
-void MSDradix(void *array, int size_a, int size_e, int max, int (*compare)(void *, void *)) {
-	uint64_t bit = 1ULL << (uint64_t) max;
+void MSDradix(void *array, int size_a, int size_e, int bit , int (*compare)(void *, void *)) {
+	//uint64_t bit = 1ULL << (uint64_t) max;
 
+	int left = 0;
+	int right = size_a - 1;
+
+	while(right > left) {
+		for(; (left < right) && compare(&bit, array + left * size_e); ++left);
+		for(; (right > left) && !compare(&bit, array + right * size_e); --right);
+
+		swap(array + left * size_e,
+		     array + right * size_e,
+		     size_e);
+	}
+
+	/*
 	int writeHead = size_a - 1;
 	for(int readHead = size_a - 1; readHead >= 0; --readHead) {
 		if(compare(&bit, array + readHead * size_e)) {
@@ -38,7 +51,7 @@ void MSDradix(void *array, int size_a, int size_e, int max, int (*compare)(void 
 			--writeHead;
 		}
 	}
-
+	
 	if(max > 0) {
 		if(writeHead)
 			MSDradix(array, writeHead + 1, size_e, max - 1, compare);
@@ -46,11 +59,20 @@ void MSDradix(void *array, int size_a, int size_e, int max, int (*compare)(void 
 		if(size_a - writeHead - 1)
 			MSDradix(array + (writeHead + 1) * size_e, size_a - writeHead - 1, size_e, max - 1, compare);
 	}
+	*/
+
+	if(bit > 0) {
+		if(right)
+			MSDradix(array, right + 1, size_e, bit >> 1, compare);
+
+		if(size_a - right - 1)
+			MSDradix(array + (right + 1) * size_e, size_a - right - 1, size_e, bit >> 1, compare);
+	}
 }
 
 
 void sort(void *array, int size_a, int size_e, int (*compare)(void *, void *)) {
 	int max = highest_bit(array, size_a, size_e, compare);
-
-	MSDradix(array, size_a, size_e, max, compare);
+	int bit = 1ULL << max;
+	MSDradix(array, size_a, size_e, bit, compare);
 }
