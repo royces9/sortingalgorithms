1a385d1def23ca5890548117109deafe04b1eb14
Royce <rrysato@gmail.com>
2019-02-14 16:41:33 -0800

    quicksort fixed, kinda ugly though

--- sort/quicksort.c
+++ sort/quicksort.c
@@ -6,13 +6,17 @@ void sort_r(void *array, int size_a, int size_e, int (*compare)(void *, void *))
 	int left = 1;
 	int right = size_a - 1;
 
-	void *pivot = array + (size_a / 2) * size_e;
+	void *pivot = array + (right / 2) * size_e;
 	void *end = array + right * size_e;
 
-	if(compare(array, pivot) ^ compare(pivot, end))
-		pivot = compare(array, end) ? end : array;
+	int comp = compare(array, pivot);
 
-	swap(array, pivot, size_e);
+	if(comp ^ compare(pivot, end)) {
+		if(!(compare(array, end) ^ comp))
+			swap(array, end, size_e);
+	} else {
+		swap(array, pivot, size_e);
+	}
 	
 	while(right > left) {
 		for(; (left < right) && compare(array, array + left * size_e); ++left);
@@ -23,25 +27,21 @@ void sort_r(void *array, int size_a, int size_e, int (*compare)(void *, void *))
 		     size_e);
 	}
 
-	
-	if(compare(array, array + left * size_e))
-		swap(array, array + (left - 1) * size_e, size_e);
+	swap(array, array + (left - 1) * size_e, size_e);
 
-	if((left - 1) > 1)
+	if((left - 1) > 2) {
 		sort_r(array, left - 1, size_e, compare);
+	} else if((left - 1) > 1) {
+		if(compare(array, array + size_e))
+			swap(array, array + size_e, size_e);
+	}
 	
-	if((size_a - left) > 1)
-		sort_r(array + left * size_e, size_a - left - 1, size_e, compare);
-	/*
-	if(compare(array, array + left * size_e))
-		swap(array, array + left * size_e, size_e);
-
-	if(left > 1)
-		sort_r(array, left, size_e, compare);
-
-	if((size_a - left) > 1)
- 		sort_r(array + left * size_e, size_a - left, size_e, compare);
-	*/
+	if((size_a - left) > 2) {
+		sort_r(array + left * size_e, size_a - left, size_e, compare);
+	} else if((size_a - left) > 1) {
+		if(compare(array + left * size_e, array + (left + 1) * size_e))
+			swap(array + left * size_e, array + (left + 1) * size_e, size_e);
+	}
 }
 
 
