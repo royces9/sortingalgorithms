a60dc8d93c5e5854c2d894aa1b057b7ec4954ff8
royces9 <rrysato@gmail.com>
2019-02-19 13:24:03 -0800

    minor change to insertion sort

--- sort/inplace_merge.c
+++ sort/inplace_merge.c
@@ -3,8 +3,8 @@
 #include "swap.h"
 
 void insertion(void *array, int size_a, int size_e, int (*compare)(void *, void *)) {
-	for(int i = 0; i < size_a; ++i) {
-                for(int j = i; (j >= 1) && compare(array + (j - 1) * size_e, array + j * size_e); --j) {
+	for(int i = 1; i < size_a; ++i) {
+                for(int j = i; (j > 0) && compare(array + (j - 1) * size_e, array + j * size_e); --j) {
 			swap(array + (j - 1) * size_e, array + j * size_e, size_e);
 		}
 	}
--- sort/insertionsort.c
+++ sort/insertionsort.c
@@ -4,8 +4,8 @@
 
 //insertion sort
 void sort(void *array, int size_a, int size_e, int (*compare)(void *, void *)) {
-	for(int i = 0; i < size_a; ++i) {
-		for(int j = i; (j >= 1) && compare(array + (j - 1) * size_e, array + j * size_e); --j) {
+	for(int i = 1; i < size_a; ++i) {
+		for(int j = i; (j > 0) && compare(array + (j - 1) * size_e, array + j * size_e); --j) {
 			swap(array + (j - 1) * size_e, array + j * size_e, size_e);
 		}
 	}
