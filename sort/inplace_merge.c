495bb302ee1ef9a2753a3f468475236252ff770c
Royce <rrysato@gmail.com>
2019-02-20 13:59:34 -0800

    try to optimize a little bit

--- sort/inplace_merge.c
+++ sort/inplace_merge.c
@@ -19,12 +19,10 @@ int divide_two(int a) {
 }
 
 
-void swap_cycle(void *array, int start_ind, int orig_ind, int l_size, int size_e) {
-	//current location
-	int cur_ind = divide_two(orig_ind);
-
+void swap_cycle(void *array, int start_ind, int cur_ind, int l_size, int size_e) {
 	//new location
 	int new_ind = start_ind;
+
 	while(cur_ind != start_ind) {
 		swap(array + cur_ind * size_e, array + new_ind * size_e, size_e);
 
@@ -34,8 +32,9 @@ void swap_cycle(void *array, int start_ind, int orig_ind, int l_size, int size_e
 }
 
 
-int next_in_order(void *array, int cur_ind, int new_ind, int size_e, int(*compare)(void *, void *)) {
-	return !compare(array + cur_ind * size_e, array + new_ind * size_e) ^ (cur_ind > new_ind);
+int next_in_order(void *array, int cur_ind, int new_ind, int size_e, int (*compare)(void *, void *)) {
+	//equivalent to !(compare(cur, new) ^ (cur > new));
+	return compare(array + cur_ind * size_e, array + new_ind * size_e) ^ (cur_ind < new_ind);
 }
 
 
@@ -49,6 +48,21 @@ int in_order(void *array, int index, int size, int size_e, int (*compare)(void *
 }
 
 
+int cycle_order(void *array, int start_ind, int cur_ind, int l_size, int size_e, int (*compare)(void *, void *)) {
+	int new_ind = start_ind;
+
+	while(cur_ind != start_ind) {
+
+		new_ind = cur_ind;
+		cur_ind = divide_two(((new_ind - 1) / 2) + l_size);
+
+		if(!next_in_order(array, cur_ind, new_ind, size_e, compare))
+			return 0;
+	}
+
+	return 1;
+}
+
 //mergesort
 //inplace merge
 void merge(void *array, int l_size, int r_size, int size_e, int (*compare)(void *, void *)) {
@@ -59,15 +73,25 @@ void merge(void *array, int l_size, int r_size, int size_e, int (*compare)(void 
 		swap(array + i * size_e, array + new_ind * size_e, size_e);
 	}
 
-
-	//for(int new_ind = 1; new_ind < size ; new_ind += 2) {
-	for(int new_ind = 1; (!in_order(array, new_ind, size, size_e, compare)) && (new_ind < size) ; new_ind += 2) {
+#if 0
+	for(int new_ind = 1; new_ind < size; new_ind += 2) {
 		int orig_ind = (new_ind - 1) / 2 + l_size;
-		if(!next_in_order(array, new_ind, divide_two(orig_ind), size_e, compare)) {
-			swap_cycle(array, new_ind, orig_ind, l_size, size_e);
+		int cur_ind = divide_two(orig_ind);
+
+		if(!cycle_order(array, new_ind, cur_ind, l_size, size_e, compare)) {
+			swap_cycle(array, new_ind, cur_ind, l_size, size_e);
 		}
 	}
+#else
+	for(int new_ind = 1; !in_order(array, new_ind, size, size_e, compare); new_ind += 2) {
+		int orig_ind = (new_ind - 1) / 2 + l_size;
+		int cur_ind = divide_two(orig_ind);
 
+		if(!next_in_order(array, new_ind, cur_ind, size_e, compare)) {
+			swap_cycle(array, new_ind, cur_ind, l_size, size_e);
+		}
+	}
+#endif
 	insertion(array, size, size_e, compare);
 }
 
