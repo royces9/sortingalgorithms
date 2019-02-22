8c4cf2f7d43cfa3d5178aed69791939ebf9238ab
Royce <rrysato@gmail.com>
2019-02-21 17:27:49 -0800

    simplify

--- sort/inplace_merge.c
+++ sort/inplace_merge.c
@@ -38,31 +38,6 @@ int next_in_order(void *array, int cur_ind, int new_ind, int size_e, int (*compa
 }
 
 
-int in_order(void *array, int index, int size, int size_e, int (*compare)(void *, void *)) {
-	for(int i = index + 4; i < size; i += 2) {
-		if(compare(array + (i - 2) * size_e, array + i * size_e))
-			return 0;
-	}
-
-	return 1;
-}
-
-
-int cycle_order(void *array, int start_ind, int cur_ind, int l_size, int size_e, int (*compare)(void *, void *)) {
-	int new_ind = start_ind;
-
-	while(cur_ind != start_ind) {
-
-		new_ind = cur_ind;
-		cur_ind = divide_two(((new_ind - 1) / 2) + l_size);
-
-		if(!next_in_order(array, cur_ind, new_ind, size_e, compare))
-			return 0;
-	}
-
-	return 1;
-}
-
 //mergesort
 //inplace merge
 void merge(void *array, int l_size, int r_size, int size_e, int (*compare)(void *, void *)) {
@@ -73,8 +48,6 @@ void merge(void *array, int l_size, int r_size, int size_e, int (*compare)(void 
 		swap(array + i * size_e, array + new_ind * size_e, size_e);
 	}
 
-#if 1
-	//this one is better so far
 	for(int new_ind = 1; new_ind < size; new_ind += 2) {
 		int orig_ind = (new_ind - 1) / 2 + l_size;
 		int cur_ind = divide_two(orig_ind);
@@ -83,17 +56,7 @@ void merge(void *array, int l_size, int r_size, int size_e, int (*compare)(void 
 			swap_cycle(array, new_ind, cur_ind, l_size, size_e);
 		}
 	}
-#else
-	//a little slower?
-	for(int new_ind = 1; !in_order(array, new_ind, size, size_e, compare); new_ind += 2) {
-		int orig_ind = (new_ind - 1) / 2 + l_size;
-		int cur_ind = divide_two(orig_ind);
 
-		if(!next_in_order(array, new_ind, cur_ind, size_e, compare)) {
-			swap_cycle(array, new_ind, cur_ind, l_size, size_e);
-		}
-	}
-#endif
 	insertion(array, size, size_e, compare);
 }
 
