b1443b8d7613c40ff47b2ebb1baa346c3fba1266
Royce <rrysato@gmail.com>
2019-02-25 20:58:12 -0800

    made labels

--- plot.py
+++ plot.py
@@ -8,7 +8,7 @@ import sys
 
 def plot_file(file_name):
     data = numpy.loadtxt(file_name);
-    matplotlib.pyplot.plot(data[:, 0], data[:, 1], label = file);
+    matplotlib.pyplot.plot(data[:, 0], data[:, 1], label=file_name);
 
 
 if __name__ == "__main__":
@@ -20,10 +20,10 @@ if __name__ == "__main__":
 
     fig = matplotlib.pyplot.figure();
 
-    for f in sys.argv[3:-1]:
+    for f in sys.argv[3:]:
         plot_file(f);
 
     matplotlib.pyplot.xlabel(xlab);
     matplotlib.pyplot.ylabel(ylab);
-    matplotlib.pyplot.legend(sys.argv[3:-1]);
-    matplotlib.show();
+    matplotlib.pyplot.legend();
+    matplotlib.pyplot.show();
