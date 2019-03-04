78bc90a331de2c02d7f520a4a421b7c8dc01b8f6
royces9 <rrysato@gmail.com>
2019-03-04 11:54:03 -0800

    add quicksort_thread

---
+++ sort/quicksort_thread.c
@@ -0,0 +1,86 @@
+#include <stdlib.h>
+#include <pthread.h>
+
+#include "shuffle.h"
+#include "swap.h"
+
+
+struct thread_data {
+	void *array;
+	int (*compare)(void *, void *);
+	int size_a;
+	int size_e;
+	int count;
+};
+
+
+void sort_r(void *inp) {
+	struct thread_data data = *(struct thread_data *) inp;
+
+	void *array = data.array;
+	int (*compare)(void *, void *) = data.compare;
+	int size_a = data.size_a;
+	int size_e = data.size_e;
+	int count = data.count;
+
+	int left = 1;
+	int right = size_a - 1;
+
+	void *pivot = array + (right / 2) * size_e;
+	void *end = array + right * size_e;
+
+	int comp = compare(array, pivot);
+
+	if(comp ^ compare(pivot, end)) {
+		if(!(compare(array, end) ^ comp))
+			swap(array, end, size_e);
+	} else {
+		swap(array, pivot, size_e);
+	}
+
+	while(right > left) {
+		for(; (left < right) && compare(array, array + left * size_e); ++left);
+		for(; (right > left) && compare(array + right * size_e, array); --right);
+
+		swap(array + left * size_e,
+		     array + right * size_e,
+		     size_e);
+	}
+
+	swap(array, array + (left - 1) * size_e, size_e);
+
+	if((left - 1) > 2) {
+
+		if(count) {
+			pthread_create();
+		} else {
+			sort_r(array, left - 1, size_e, compare);
+		}
+
+
+	} else if((left - 1) > 1) {
+		if(compare(array, array + size_e))
+			swap(array, array + size_e, size_e);
+	}
+
+	if((size_a - left) > 2) {
+		sort_r(array + left * size_e, size_a - left, size_e, compare);
+	} else if((size_a - left) > 1) {
+		if(compare(array + left * size_e, array + (left + 1) * size_e))
+			swap(array + left * size_e, array + (left + 1) * size_e, size_e);
+	}
+}
+
+
+void sort(void *array, int size_a, int size_e, int (*compare)(void *, void *), void *extra) {
+	int count = 2;
+	if(extra)
+		count = *(int *) extra;
+	
+	if(size_a != 1) {
+		struct thread_data data = {array, compare, size_a, size_e, count};
+		sort_r(&data);
+	}
+}
+
+
